274cd881adc2bf45a3a159319c2c12a9916983f6
madhupriya <madpriy1@in.ibm.com>
2018-12-31 16:17:41 +0530

    testcase pe4 initial commit

---
+++ src/main/java/com/stackroute/pefour/App.java
@@ -0,0 +1,13 @@
+package com.stackroute.pefour;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
